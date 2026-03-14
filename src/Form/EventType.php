23cf2633f61e7aa76e9e1c9e33456758afb4d103
Amine Dardouri <dardourimohamedamine1@gmail.com>
2026-03-15 00:50:29 +0100

    création des formulaires Symfony

---
+++ src/Form/EventType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Titre de l\'événement']
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => ['class' => 'form-control', 'rows' => 5, 'placeholder' => 'Description détaillée...']
+            ])
+            ->add('date', DateTimeType::class, [
+                'label' => 'Date et heure',
+                'widget' => 'single_text',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('location', TextType::class, [
+                'label' => 'Lieu',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Lieu de l\'événement']
+            ])
+            ->add('seats', IntegerType::class, [
+                'label' => 'Nombre de places',
+                'attr' => ['class' => 'form-control', 'min' => 1]
+            ])
+            ->add('imageFile', FileType::class, [
+                'label' => 'Image (JPG, PNG)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'mimeTypes' => ['image/jpeg', 'image/png', 'image/webp'],
+                        'mimeTypesMessage' => 'Veuillez uploader une image valide (JPG, PNG, WEBP)',
+                    ])
+                ],
+                'attr' => ['class' => 'form-control']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
